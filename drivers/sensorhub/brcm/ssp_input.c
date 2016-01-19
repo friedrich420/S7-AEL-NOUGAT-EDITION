05ca01cb304cb0227fd3f5fff50d5c0ff143a90e
halaszk <halaszk@deathlord.eu>
2016-01-19 07:39:00 +0100

    sensorhub: brcm: add wakelock timeout controls
    Signed-off-by: Tkkg1994 <luca.grifo@outlook.com>
    
    Conflicts:
    	drivers/sensorhub/brcm/ssp_input.c
    	drivers/sensorhub/brcm/ssp_sensorhub.h

--- drivers/sensorhub/brcm/ssp_input.c
+++ drivers/sensorhub/brcm/ssp_input.c
@@ -20,6 +20,13 @@
 #include <linux/iio/buffer.h>
 #include <linux/iio/types.h>
 
+#include <linux/moduleparam.h>
+
+static int wl_prox = 1;
+module_param(wl_prox, int, 0644);
+static int wl_humi = 2;
+module_param(wl_humi, int, 0644);
+
 /*************************************************************************/
 /* SSP Kernel -> HAL input evnet function                                */
 /*************************************************************************/
@@ -474,7 +481,11 @@ void report_prox_data(struct ssp_data *data, struct sensor_value *proxdata)
 		ts_low);
 	input_sync(data->prox_input_dev);
 
+<<<<<<< HEAD
 	wake_lock_timeout(&data->ssp_wake_lock, 0.3*HZ);
+=======
+	wake_lock_timeout(&data->ssp_wake_lock, wl_prox * HZ);
+>>>>>>> dfc4df0... sensorhub: brcm: add wakelock timeout controls
 }
 
 void report_prox_raw_data(struct ssp_data *data,
@@ -592,7 +603,11 @@ void report_temp_humidity_data(struct ssp_data *data,
 		data->buf[TEMPERATURE_HUMIDITY_SENSOR].y);
 	input_sync(data->temp_humi_input_dev);
 	if (data->buf[TEMPERATURE_HUMIDITY_SENSOR].z)
+<<<<<<< HEAD
 		wake_lock_timeout(&data->ssp_wake_lock, 0.3*HZ);
+=======
+		wake_lock_timeout(&data->ssp_wake_lock, wl_humi * HZ);
+>>>>>>> dfc4df0... sensorhub: brcm: add wakelock timeout controls
 }
 
 #ifdef CONFIG_SENSORS_SSP_SHTC1
